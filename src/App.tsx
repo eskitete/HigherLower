eaa98e73dde5a77d2ad64732a8fce1382e819770
Rafay Syed <65063591+eskitete@users.noreply.github.com>
2025-04-02 11:22:26 -0500

    Add NFL game mode with player data and game logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home';
 import NBAGame from './games/NBAGame';
+import NFLGame from './games/NFLGame';
 import ComingSoon from './components/ComingSoon';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/nba" element={<NBAGame />} />
-        <Route path="/nfl" element={<ComingSoon sport="NFL" />} />
+        <Route path="/nfl" element={<NFLGame />} />
         <Route path="/soccer" element={<ComingSoon sport="Soccer" />} />
         <Route path="/ufc" element={<ComingSoon sport="UFC" />} />
         <Route path="/f1" element={<ComingSoon sport="F1" />} />
